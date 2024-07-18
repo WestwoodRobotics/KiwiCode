1ccf8da7d5ae65c6618864f310f90015a239ba1a
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-07-18 02:10:54 +0000

    trying to create a devcontainer and action workflow

---
+++ .devcontainer/post_create.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+yarn install --network-timeout 180000
+yarn electron
+
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+cd ..
+
+cd WPILib_Linux-2024.3.2
+
+./WPILibInstaller
