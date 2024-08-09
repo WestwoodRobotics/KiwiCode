3103f7667d564d3cc35d99b2fc6cad53268beb27
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-08-09 02:39:10 +0000

    Pending changes exported from your codespace

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,42 +1,56 @@
 #!/bin/sh
 
-apt-get update && apt-get install -y sudo && echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo "Post-create script starting..."
 
 # Check if wget is installed
 if ! command -v wget &> /dev/null
 then
     echo "wget could not be found, installing..."
-    apt-get update && apt-get install -y wget
+    sudo apt update && apt install -y wget
 else
-    echo "wget is already installed"
+    sudo echo "wget is already installed"
 fi
 
 # Check if openjdk-21-jre is installed
 if ! dpkg -l | grep -q openjdk-21-jre
 then
     echo "openjdk-21-jre could not be found, installing..."
-    apt install -y openjdk-21-jre
+    sudo apt install -y openjdk-21-jre
 else
-    echo "openjdk-21-jre is already installed"
+    sudo echo "openjdk-21-jre is already installed"
 fi
 
 # Extract year and version from the URL
 URL="https://packages.wpilib.workers.dev/installer/v2024.3.2/Linux/WPILib_Linux-2024.3.2.tar.gz"
 VERSION=$(echo $URL | grep -oP 'v\K[0-9]{4}\.[0-9]+\.[0-9]+')
+YEAR=${VERSION%%.*}
 
 # Check if WPILib is already set up
 if [ ! -d "$HOME/wpilib/${VERSION%%.*}" ]; then
-    wget $URL
-    tar -xzvf WPILib_Linux-$VERSION.tar.gz
-    mkdir ${VERSION%%.*}
+    echo "Downloading WPILib..."
+    sudo wget $URL
+    echo "Extracting WPILib..."
+    sudo tar -xzvf WPILib_Linux-$VERSION.tar.gz
+    echo "Setting up WPILib directory..."
+    sudo mkdir ${VERSION%%.*}
     cd ${VERSION%%.*}
-    mv ../WPILib_Linux-$VERSION/WPILib_Linux-$VERSION-artifacts.tar.gz .
-    tar xf WPILib_Linux-$VERSION-artifacts.tar.gz
-    rm WPILib_Linux-$VERSION-artifacts.tar.gz
+    sudo mv ../WPILib_Linux-$VERSION/WPILib_Linux-$VERSION-artifacts.tar.gz .
+    echo "Extracting WPILib artifacts..."
+    sudo tar xf WPILib_Linux-$VERSION-artifacts.tar.gz
+    sudo rm WPILib_Linux-$VERSION-artifacts.tar.gz
     cd ..
-    mkdir -p ~/wpilib
-    mv ${VERSION%%.*} ~/wpilib
-    rm -rf WPILib_Linux-*
+    sudo mkdir -p ~/wpilib
+    sudo mv ${VERSION%%.*} ~/wpilib
+    sudo rm -rf WPILib_Linux-*
+    # Run ToolsUpdater.py
+    cd ~/wpilib/$YEAR/tools/ && python3 ToolsUpdater.py
+
+    # Install VS Code extensions
+    cd ~/wpilib/$YEAR/vsCodeExtensions && find . -name "*.vsix" | xargs -I {} code --install-extension {}
+
+
 else
     echo "WPILib is already set up"
 fi
+
+echo "Post-create script finished."
