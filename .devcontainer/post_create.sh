559ac8bd75866cfb73ce0bf6bcfb2f7fbec7a959
SkywardSyntax <87048477+SkywardSyntax@users.noreply.github.com>
2024-10-24 23:32:26 +0000

    Install OpenJDK 21 in post-create script; clean up WPILib setup logic

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,24 +2,15 @@
 
 echo "Post-create script starting..."
 
+# Install OpenJDK 21
+echo "Installing OpenJDK 21..."
+sudo apt-get update
+sudo apt-get install -y openjdk-21-jre
+
 # Extract year and version from the URL
 URL="https://packages.wpilib.workers.dev/installer/v2024.3.2/Linux/WPILib_Linux-2024.3.2.tar.gz"
 VERSION=$(echo $URL | grep -oP 'v\K[0-9]{4}\.[0-9]+\.[0-9]+')
 YEAR=${VERSION%%.*}
-
-# # Check if WPILib is already set up
-# if [ ! -d "$HOME/wpilib/${VERSION%%.*}" ]; then
-#     echo "Downloading WPILib..."
-#     sudo wget $URL
-#     echo "Extracting WPILib..."
-#     sudo tar -xzvf WPILib_Linux-$VERSION.tar.gz
-#     echo "Running WPILibInstaller..."
-#     cd WPILib_Linux-$VERSION
-#     sudo chmod +x WPILibInstaller
-    
-# else
-#     echo "WPILib is already set up"
-# fi
 
 # Check if WPILib is already set up
 if [ ! -d "$HOME/wpilib/${VERSION%%.*}" ]; then
